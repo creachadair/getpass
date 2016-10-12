c08aa91f7200fcec7a0a3c07fef0420cb93b0ee0
M. J. Fromberger <michael.j.fromberger@gmail.com>
2016-10-12 15:56:44 -0700

    Add an example command-line tool.

---
+++ cmd/getpass/getpass.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"bitbucket.org/creachadair/getpass"
+)
+
+var prompt = flag.String("prompt", "Password: ", "Prompt string")
+
+func main() {
+	flag.Parse()
+	pw, err := getpass.Prompt(*prompt)
+	if err != nil {
+		log.Fatalf("getpass: %v", err)
+	}
+	fmt.Println(pw)
+}
