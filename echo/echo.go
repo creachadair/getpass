9ca2d24869e450d813069e00b1296a1bf93c6110
M. J. Fromberger <michael.j.fromberger@gmail.com>
2016-10-11 17:52:04 -0700

    Add echo package.

---
+++ echo/echo.go
@@ -0,0 +1,59 @@
+// Package echo allows enabling and disabling terminal echo.
+package echo
+
+/*
+#include <unistd.h>
+#include <termios.h>
+*/
+import "C"
+import (
+	"errors"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+func isatty(fd uintptr) bool { return C.isatty(C.int(fd)) > 0 }
+
+func tcgetattr(fd uintptr) (*C.struct_termios, error) {
+	var info C.struct_termios
+	if ec := C.tcgetattr(C.int(fd), &info); ec < 0 {
+		return nil, errors.New("tcgetattr failed")
+	}
+	return &info, nil
+}
+
+func tcsetattr(fd uintptr, info *C.struct_termios) error {
+	if ec := C.tcsetattr(C.int(fd), C.int(unix.TCSAFLUSH), info); ec < 0 {
+		return errors.New("tcsetattr failed")
+	}
+	return nil
+}
+
+const termBits = syscall.ECHO | syscall.ECHOE | syscall.ECHOK | syscall.ECHONL
+
+// Enable enables echo for the terminal connected to descriptor fd.
+func Enable(fd uintptr) error {
+	if !isatty(fd) {
+		return syscall.ENOTTY
+	}
+	info, err := tcgetattr(fd)
+	if err != nil {
+		return err
+	}
+	info.c_lflag |= termBits
+	return tcsetattr(fd, info)
+}
+
+// Disable disables echo for the terminal connected to descriptor fd.
+func Disable(fd uintptr) error {
+	if !isatty(fd) {
+		return syscall.ENOTTY
+	}
+	info, err := tcgetattr(fd)
+	if err != nil {
+		return err
+	}
+	info.c_lflag &^= termBits
+	return tcsetattr(fd, info)
+}
