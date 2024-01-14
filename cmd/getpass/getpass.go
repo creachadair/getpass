9cad29db84a66daafaa7a9973dc141b0bd56c2c8
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-01-13 22:04:20 -0800

    cmd/getpass: add a --confirm flag

--- cmd/getpass/getpass.go
+++ cmd/getpass/getpass.go
@@ -8,13 +8,24 @@ import (
 	"github.com/creachadair/getpass"
 )
 
-var prompt = flag.String("prompt", "Password: ", "Prompt string")
+var (
+	prompt    = flag.String("prompt", "Password: ", "Prompt string")
+	doConfirm = flag.Bool("confirm", false, "Prompt for confirmation")
+)
 
 func main() {
 	flag.Parse()
 	pw, err := getpass.Prompt(*prompt)
 	if err != nil {
 		log.Fatalf("getpass: %v", err)
+	}
+	if *doConfirm {
+		cf, err := getpass.Prompt("(confirm) " + *prompt)
+		if err != nil {
+			log.Fatalf("get confirmation: %v", err)
+		} else if cf != pw {
+			log.Fatal("Values do not match")
+		}
 	}
 	fmt.Println(pw)
 }
